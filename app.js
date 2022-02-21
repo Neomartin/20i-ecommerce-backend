2cdebf3e27ae8bd83ad85b523498fef7f6c918a6
Ivan Roldan <ivanroldan1989@gmail.com>
2022-02-21 18:54:01 -0300

    Comentario agregado

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var userController = require('./controllers/user.controller');
 
-app.use(express.json()); // Funcion para poder leer los valores enviados en metodos m
+app.use(express.json()); // Funcion para poder leer los valores enviados en metodos del body
 app.use(express.urlencoded({extended: true}));
 
 app.get('/api/user', userController.getUsers)
