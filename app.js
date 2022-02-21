515c0948b18d198a37a120ef5368ff400710524b
Martín Rodríguez <neomartinr@gmail.com>
2022-02-21 20:37:11 -0300

    Find user by id

--- app.js
+++ app.js
@@ -5,8 +5,22 @@ var userController = require('./controllers/user.controller');
 app.use(express.json()); // Función para poder leer los valores enviados en métodos post mediante el body
 app.use(express.urlencoded({extended: true}));
 
-app.get('/api/user', userController.getUsers);
+app.get('/api/users', userController.getUsers);
 app.post('/api/user', userController.addUser);
+// app.delete('api/use')
+
+
+
+app.get('/api/user', userController.getUser);
+app.delete
+
+//1- Generar un endpoint con el método delete
+//2- Asociar el endpoint a un contralador (función)
+//3- Definir esa función en mis controladores
+//4- Exportarla para poder usarla en mi app.js
+//5- Recibir de alguna manera el ID del user que quiero borrar
+//6- Buscar ese id en mongo y eliminarlo con la función asociada al Schema user .findByIdAndRemove
+//7- Devolver una respuesta
 
 
 module.exports = app;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,7 +11,23 @@ async function getUsers(req, res) {
     res.send({ users: usuariosDB })
 }
 
+async function getUser(req, res) {
+    //Id que recibimos como query param desde el endpoint
+    const userId = req.query.user_id;
+    //Buscamos específicamente ese id en nuestra collección users
+    const user = await User.findById(userId);
+    console.log(user)
+    // Si no encontramos el usuario
+    if(user == null) return res.status(404).send('No se encontró el usuario que busca');
+
+    return res.status(200).send({ user: user });
+}
+
+
 module.exports = {
     addUser,
     getUsers,
+    getUser
 }
+
+// fetch(`localhost:3000/api/user/?user_id=${id}`)
